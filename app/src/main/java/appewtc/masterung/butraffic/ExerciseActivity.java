8ec1325b639d91f6f75d1f2136385ddd8c1b961a
masterUNG <phrombutr@gmail.com>
2015-11-15 15:13:29 +0700

    Change Question

--- app/src/main/java/appewtc/masterung/butraffic/ExerciseActivity.java
+++ app/src/main/java/appewtc/masterung/butraffic/ExerciseActivity.java
@@ -1,13 +1,68 @@
 package appewtc.masterung.butraffic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class ExerciseActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView questionTextView;
+    private ImageView trafficImageView;
+    private RadioGroup choiceRadioGroup;
+    private RadioButton choice1RadioButton, choice2RadioButton,
+            choice3RadioButton, choice4RadioButton;
+    private int timesAnInt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+
+        bindWidget();
+
+    }   // Main Method
+
+    public void clickAnswer(View view) {
+
+        String[] strQuestion = new String[5];
+        strQuestion[0] = "1. What is this";
+        strQuestion[1] = "2. What is this";
+        strQuestion[2] = "3. What is this";
+        strQuestion[3] = "4. What is this";
+        strQuestion[4] = "5. What is this";
+
+        timesAnInt += 1;
+
+        if (timesAnInt < 5) {
+
+            questionTextView.setText(strQuestion[timesAnInt]);
+
+        } else {
+
+            startActivity(new Intent(ExerciseActivity.this, ScoreActivity.class));
+
+        }
+
+
+
+    }   // clickAnswer
+
+
+    private void bindWidget() {
+        questionTextView = (TextView) findViewById(R.id.textView5);
+        trafficImageView = (ImageView) findViewById(R.id.imageView3);
+        choiceRadioGroup = (RadioGroup) findViewById(R.id.radChoice);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
     }
-}
+
+
+}   // Main Class
