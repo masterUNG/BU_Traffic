68dd2d894dd7ad46c05c5f8ff626b3769bd662d2
masterUNG <phrombutr@gmail.com>
2015-11-15 11:12:30 +0700

    ListView Finish

--- app/src/main/java/appewtc/masterung/butraffic/MyAdapter.java
+++ app/src/main/java/appewtc/masterung/butraffic/MyAdapter.java
@@ -1,9 +1,12 @@
 package appewtc.masterung.butraffic;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by masterUNG on 11/14/15 AD.
@@ -23,7 +26,7 @@ public class MyAdapter extends BaseAdapter{
 
     @Override
     public int getCount() {
-        return 0;
+        return titleStrings.length;
     }
 
     @Override
@@ -38,6 +41,18 @@ public class MyAdapter extends BaseAdapter{
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.my_listview, viewGroup, false);
+
+        //Setup Title
+        TextView titleTextView = (TextView) objView1.findViewById(R.id.textView2);
+        titleTextView.setText(titleStrings[i]);
+
+        //Setup Icon
+        ImageView iconImageView = (ImageView) objView1.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[i]);
+
+        return objView1;
     }
 }   // Main Class
